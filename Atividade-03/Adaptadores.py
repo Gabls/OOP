6f4645d5af6c9e0a7ec0ddb4432d772dc4cf7328
Gabriel Santos de Sousa <gablsimportante@gmail.com>
2024-04-03 15:46:35 -0300

    fix: change parameters order

--- Atividade-03/Adaptadores.py
+++ Atividade-03/Adaptadores.py
@@ -8,13 +8,13 @@ class SMS_Adapter(Notifier_Interface):
     def __init__(self, sms: SMS) -> None:
         self.sms = sms
 
-    def send_message(self, message: str, recipent: str) -> bool:
-        return self.sms.send_sms(message, recipent)
+    def send_message(self, recipent: str, message: str) -> bool:
+        return self.sms.send_sms(recipent, message)
 
 # Adaptador - Notificador -> Push
 class Push_Adapter(Notifier_Interface):
     def __init__(self, push: Push) -> None:
         self.push = push
 
-    def send_message(self, message: str, recipent: str) -> bool:
-        return self.push.send_push(message, recipent)
+    def send_message(self, recipent: str, message: str) -> bool:
+        return self.push.send_push(recipent, message)
